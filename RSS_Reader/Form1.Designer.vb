f1ae37a09c570e9690769fc0f3550c85d3551cfd
cook green <2721595960@qq.com>
2015-01-31 09:12:25 +0800

    first commit

---
+++ RSS_Reader/Form1.Designer.vb
@@ -0,0 +1,232 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tRssAddress = New System.Windows.Forms.TextBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.LinkLabel3 = New System.Windows.Forms.LinkLabel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lLink = New System.Windows.Forms.Label()
+        Me.lDes = New System.Windows.Forms.Label()
+        Me.lTiltle = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(434, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "load RSS"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(14, 164)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(194, 160)
+        Me.ListBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "RSS Address:"
+        '
+        'tRssAddress
+        '
+        Me.tRssAddress.Location = New System.Drawing.Point(105, 24)
+        Me.tRssAddress.Name = "tRssAddress"
+        Me.tRssAddress.Size = New System.Drawing.Size(300, 21)
+        Me.tRssAddress.TabIndex = 3
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(226, 181)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(299, 143)
+        Me.WebBrowser1.TabIndex = 4
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.LinkLabel1.Location = New System.Drawing.Point(226, 164)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(67, 14)
+        Me.LinkLabel1.TabIndex = 5
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "LinkLabel1"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.LinkLabel3)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.LinkLabel2)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.lLink)
+        Me.GroupBox1.Controls.Add(Me.lDes)
+        Me.GroupBox1.Controls.Add(Me.lTiltle)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 58)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(511, 94)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Channel Properties"
+        '
+        'LinkLabel3
+        '
+        Me.LinkLabel3.AutoSize = True
+        Me.LinkLabel3.Location = New System.Drawing.Point(129, 67)
+        Me.LinkLabel3.Name = "LinkLabel3"
+        Me.LinkLabel3.Size = New System.Drawing.Size(65, 12)
+        Me.LinkLabel3.TabIndex = 8
+        Me.LinkLabel3.TabStop = True
+        Me.LinkLabel3.Text = "LinkLabel3"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(130, 41)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 12)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Label3"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(130, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 12)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Label2"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(104, 67)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(0, 12)
+        Me.LinkLabel2.TabIndex = 5
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(104, 41)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(0, 12)
+        Me.Label6.TabIndex = 4
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(104, 17)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 12)
+        Me.Label5.TabIndex = 3
+        '
+        'lLink
+        '
+        Me.lLink.AutoSize = True
+        Me.lLink.Location = New System.Drawing.Point(17, 67)
+        Me.lLink.Name = "lLink"
+        Me.lLink.Size = New System.Drawing.Size(29, 12)
+        Me.lLink.TabIndex = 2
+        Me.lLink.Text = "Link"
+        '
+        'lDes
+        '
+        Me.lDes.AutoSize = True
+        Me.lDes.Location = New System.Drawing.Point(17, 41)
+        Me.lDes.Name = "lDes"
+        Me.lDes.Size = New System.Drawing.Size(71, 12)
+        Me.lDes.TabIndex = 1
+        Me.lDes.Text = "Description"
+        '
+        'lTiltle
+        '
+        Me.lTiltle.AutoSize = True
+        Me.lTiltle.Location = New System.Drawing.Point(17, 17)
+        Me.lTiltle.Name = "lTiltle"
+        Me.lTiltle.Size = New System.Drawing.Size(35, 12)
+        Me.lTiltle.TabIndex = 0
+        Me.lTiltle.Text = "Title"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(534, 342)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.tRssAddress)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "RSS Reader"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents tRssAddress As System.Windows.Forms.TextBox
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents lLink As System.Windows.Forms.Label
+    Friend WithEvents lDes As System.Windows.Forms.Label
+    Friend WithEvents lTiltle As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents LinkLabel2 As System.Windows.Forms.LinkLabel
+    Friend WithEvents LinkLabel3 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ RSS_Reader/Form1.vb
@@ -0,0 +1,91 @@
+﻿Public Class Form1
+    Dim aa As New RssItem
+    Dim arr As ArrayList
+    Dim deshtml
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If (tRssAddress.Text = "") Then
+            MsgBox("The RSS Address cannot be empty!", , "Warning")
+        Else
+            Dim rss As RssItem
+            rss = New RssItem()
+            Dim rssCh As RSSChannel
+            rssCh = New RSSChannel(tRssAddress.Text)
+            rssCh.GetChannelInfo()
+            Label2.Text = rssCh.m_Title
+            Label3.Text = rssCh.m_Description
+            LinkLabel3.Text = rssCh.m_Link
+            arr = rssCh.GetChannelItems()
+            For Each Me.aa In arr
+                ListBox1.Items.Add(aa.m_Title)
+            Next
+        End If
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
+        Dim i As Integer = ListBox1.SelectedIndex
+        aa = arr.Item(i)
+        LinkLabel1.Text = aa.m_link
+        'WebBrowser1.Refresh()
+        'WebBrowser1.Document.clear()
+        WebBrowser1.Document.Body.InnerHtml = aa.m_Description
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        WebBrowser1.Navigate("about:blank")
+    End Sub
+End Class
+Class RssItem
+    Public m_Title As String
+    Public m_link As String
+    Public m_Description As String
+    Public Sub New()
+        m_Title = ""
+        m_link = ""
+        m_Description = ""
+    End Sub
+End Class
+Class RSSChannel
+    Public m_FeedURL As String
+    Public m_Title As String
+    Public m_Link As String
+    Public m_Description As String
+    Public Sub New(ByVal url As String)
+        m_FeedURL = url
+        m_Title = ""
+        m_Link = ""
+        m_Description = ""
+        GetChannelInfo()
+    End Sub
+    Public Sub GetChannelInfo()
+        Dim rss As Xml.XmlNodeList = GetXMLDoc("rss/channel")
+        m_Title = rss(0).SelectSingleNode("title").InnerText
+        m_Link = rss(0).SelectSingleNode("link").InnerText
+        m_Description = rss(0).SelectSingleNode("description").InnerText
+    End Sub
+    Private Function GetXMLDoc(ByVal node As String) As Xml.XmlNodeList
+        Dim tempNodeList As System.Xml.XmlNodeList = Nothing
+        Dim request As System.Net.WebRequest = System.Net.WebRequest.Create(Me.m_FeedURL)
+        Dim response As System.Net.WebResponse = request.GetResponse()
+        Dim rssStream As System.IO.Stream = response.GetResponseStream()
+        Dim rssDoc As Xml.XmlDocument = New Xml.XmlDocument()
+        rssDoc.Load(rssStream)
+        tempNodeList = rssDoc.SelectNodes(node)
+        Return tempNodeList
+    End Function
+    Public Function GetChannelItems() As ArrayList
+        Dim tempArrayList As New ArrayList
+        Dim rssItems As Xml.XmlNodeList = GetXMLDoc("rss/channel/item")
+        Dim item As Xml.XmlNode
+        For Each item In rssItems
+            Dim newItem As New RSSItem
+            With newItem
+                .m_Title = item.SelectSingleNode("title").InnerText
+                .m_link = item.SelectSingleNode("link").InnerText
+                .m_Description = item.SelectSingleNode("description").InnerText
+            End With
+            tempArrayList.Add(newItem)
+        Next
+
+        Return tempArrayList
+    End Function
+End Class
